8e61eccdaa31eae3877cc8b5be80c0b020bfe8db
c#Flexer <90629888+FuckConsolePlayers@users.noreply.github.com>
2021-12-15 16:11:23 +0200

    Update genshin.js

--- genshin.js
+++ genshin.js
@@ -62,6 +62,9 @@ $.ajax({
         });
     }
 });
+var temp;
+var temp_character;
+
 function find(search){
     var s = 0;
     var el;
@@ -74,24 +77,73 @@ function find(search){
     return found;
 }
 function show(s){
+    temp = [];
     var d = find(s);
+    temp_character = d;
     var a = "";
+    var count = 0;
     d.skillTalents.forEach(element => {
-        a = a + '<h1 onclick="show_attack("'+element.name+'")" >Name of the attack: '+element.name+'</h1>'+'\n';
-        console.log(a);
+        a = a + '<h1 onclick="show_attack(temp['+count+'],temp_character)">Name of the attack: '+element.name+'</h1>'+'\n';
+        temp.push(element.name);
         a = a + '<h2>Unlock: '+element.unlock+'</h2>'+'\n';
         a = a + '<p>Description: '+element.description+'</p>'+'\n';
+        count++;
     });
     let timerInterval;
     Swal.fire({
         html: a,
-        timer: 10000,
+        timer: 25000,
         timerProgressBar: false,
         willClose: () => {
           clearInterval(timerInterval)
         }
       })
 }
-function show_attack(s){
-    console.log(s);
+function show_attack(search,character){
+    var found = character;
+    var s = 0;
+    var el;
+    var founded;
+    var temp_html = "";
+    while(search != el){
+        el = found.skillTalents[s].name;
+        founded = found.skillTalents[s];
+        s = s+1;
+    }
+    if(founded.upgrades != undefined && founded.upgrades != null){
+        temp_html = "<h1>Upgrades</h1>\n"
+        founded.upgrades.forEach(element => {
+            temp_html = temp_html + '<h2>Name: '+element.name+'</h2>'+'\n';
+            temp_html = temp_html + '<h3>Value: '+element.value+'</h3>'+'\n';
+        });
+        if(temp_html != ""){
+            let timerInterval;
+            Swal.fire({
+                html: temp_html,
+                timer: 10000,
+                timerProgressBar: false,
+                willClose: () => {
+                  clearInterval(timerInterval)
+                }
+              })
+        }
+    }
+    else{
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'error',
+            title: search+' does not have upgrades'
+          })
+    }
 }
